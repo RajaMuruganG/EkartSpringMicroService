4f8efb51eb68f2071b571ee48d05fbe812fba913
RajaMuruganG <er.raajaa@gmail.com>
2025-12-13 12:26:16 +0530

    Catalog Services

---
+++ ekart-catalog-service/src/main/java/com/productcatalog/EkartCatalogServiceApplication.java
@@ -0,0 +1,13 @@
+package com.productcatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EkartCatalogServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EkartCatalogServiceApplication.class, args);
+	}
+
+}
